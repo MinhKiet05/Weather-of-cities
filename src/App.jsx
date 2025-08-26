82251873b752bab88c231ec78dcba008eefa7a6f
MinhKiet05 <tranvanbetu1965@gmail.com>
2025-08-26 16:47:51 +0700

    push src

---
+++ src/App.jsx
@@ -0,0 +1,75 @@
+//import { useState } from 'react'
+import './App.css'
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import { useEffect } from 'react';
+import $ from 'jquery';
+//3539
+function App() {
+  const APP_ID = "01556dfe95b1b47ede86db3c42bf4ec8";
+  useEffect(() => {
+
+    $('#search-input').change(function (e) {
+      e.preventDefault();
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${e.target.value}&appid=${APP_ID}&units=metric&lang=vi`)
+        .then(async res => {
+          const data = await res.json();
+
+          if (data.cod === 200) {
+            const { name, main, weather } = data;
+            $('.city-name').text(name);
+            $('.weather-state').text(weather[0].description);
+            $('.weather-temperature').text(`${main.temp.toFixed(1)}°C`);
+            $('.sunrise').text(new Date(data.sys.sunrise * 1000).toLocaleTimeString());
+            $('.sunset').text(new Date(data.sys.sunset * 1000).toLocaleTimeString());
+            $('.wind').text(`${data.wind.speed} km/h`);
+            $('.humidity').text(`${main.humidity}%`);
+            $('.weather-icon').attr('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`);
+          } else {
+            alert('City not found');
+          }
+
+        })
+    });
+  }, []);
+
+  return (
+    <>
+      <div className="container">
+        <div className="main-section">
+          <div className="search-bar">
+            <input type="text" name='search-city' id='search-input' />
+            <i className="fa-solid fa-magnifying-glass search-icon"></i>  
+          </div>
+          <p className='city-name'>Ho Chi Minh</p>
+          <p className='weather-state'>May thua</p>
+          <img src="https://openweathermap.org/img/wn/10d@2x.png" alt="weather icon" className='weather-icon'/>
+          <p className="weather-temperature">25°C</p>
+        </div>
+        <div className="additional-section">
+          <div className="row">
+            <div className="item">
+              <div className="label">Mặt trời mọc</div>
+              <div className="value sunrise">6:14</div>
+            </div>
+            <div className="item">
+              <div className="label">Mặt trời lặn</div>
+              <div className="value sunset">18:30</div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="item">
+              <div className="label">Gió</div>
+              <div className="value wind">10 km/h</div>
+            </div>
+            <div className="item">
+              <div className="label">Độ ẩm</div>
+              <div className="value humidity">80%</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
